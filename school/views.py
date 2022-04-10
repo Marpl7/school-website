872983a2cfa407e2076e93b11eaa67436e5e7abf
Maria <marpl7@yahoo.com>
2022-04-10 20:57:04 +0200

    school website

---
+++ school/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student, Teacher
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    students = Student.objects.all()
+    teachers = Teacher.objects.all()
+    for s in students:
+        print([t.name for t in s.teachers.all()])
+
+    context = {
+        'students': students
+    }
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
